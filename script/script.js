86a20d1f7c14b41cfee887fcd4955f18cbc4a775
CBrandsDev <cbrandao2001@gmail.com>
2023-02-27 12:29:59 -0300

    some changes

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// separação das variaveis
 const songName = document.getElementById('song-name');
 const bandName = document.getElementById('band');
 const song = document.getElementById('audio');  
@@ -5,9 +6,15 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const shuffle = document.getElementById('shuffle');
+const repeat = document.getElementById('repeat');
+const progressBar = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
 
-//variavel para designar se a musica esta tocando ou nn
+// variavel para designar se a musica esta tocando ou nn
 let isPlaying = false;
+// variavel para designar se esta no aleatorio
+let isShuffle = false;
 
 // variaveis das musicas
 const fiveOrfive = {
@@ -22,17 +29,17 @@ const arabella = {
     album: 'am',
     music: 'Arabella'
 };
-const cornerstone = {
-    songName: 'Cornerstone',
-    artist: 'Arctic Monkeys',
-    album: 'humbug',
-    music: 'Cornerstone'
-};
 const cryingLightning = {
     songName: 'Crying Lightning',
     artist: 'Arctic Monkeys',
     album: 'humbug',
     music: 'Crying Lightning'
+};
+const cornerstone = {
+    songName: 'Cornerstone',
+    artist: 'Arctic Monkeys',
+    album: 'humbug',
+    music: 'Cornerstone'
 };
 const doIWannaKnow = {
     songName: 'Do I Wanna Know',
@@ -41,10 +48,10 @@ const doIWannaKnow = {
     music: 'Do I Wanna Know'
 };
 const doMeAFavor = {
-    songName: 'Do Me A Favor',
+    songName: 'Do Me A Favour',
     artist: 'Arctic Monkeys',
     album: 'favoriteworstnightmare',
-    music: 'Do Me A Favor'
+    music: 'Do Me A Favour'
 };
 const fluorescentAdolescent = {
     songName: 'Fluorescent Adolescent',
@@ -109,7 +116,8 @@ const whydYouOnlyCallMeWhenYoureHigh = {
 
 
 // array para guardar todas a musicas em uma playlist
-const playlist = [fiveOrfive,arabella,cornerstone,cryingLightning,doIWannaKnow,doMeAFavor,fluorescentAdolescent,fourOutOfFive,kneeSocks,myPropeller,oldYellowBricks,onlyOnesWhoKnow,rUMine,sculpturesOfAnythingGoes,starTreatment,whydYouOnlyCallMeWhenYoureHigh];
+const playlist = [fiveOrfive,arabella,cryingLightning,cornerstone,doIWannaKnow,doMeAFavor,fluorescentAdolescent,fourOutOfFive,kneeSocks,myPropeller,oldYellowBricks,onlyOnesWhoKnow,rUMine,sculpturesOfAnythingGoes,starTreatment,whydYouOnlyCallMeWhenYoureHigh];
+let sortedPlaylist = [...playlist];
 let index = 0;
 
 
@@ -140,17 +148,18 @@ function playPause(){
     }
 }
 
+// função para carregar as infos da musica como capa, musica, nome da musica e nome do artista
 function loadSong(){
-    cover.src = `assets/images/${playlist[index].album}.webp`;
-    song.src = `assets/songs/${playlist[index].music}.mp3`;
-    songName.innerText = playlist[index].songName;
-    bandName.innerText = playlist[index].artist;
+    cover.src = `assets/images/${sortedPlaylist[index].album}.webp`;
+    song.src = `assets/songs/${sortedPlaylist[index].music}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
 }
 
 // função para voltar a musica
 function previousSong() {
     if (index == 0) {
-        index = playlist.length -1;
+        index = sortedPlaylist.length -1;
     }
     else {
         index -= 1;
@@ -160,7 +169,7 @@ function previousSong() {
 }
 // função para avançar a musica
 function nextSong() {
-    if (index == playlist.length -1) {
+    if (index == sortedPlaylist.length -1) {
         index = 0;
     }
     else {
@@ -169,9 +178,49 @@ function nextSong() {
     loadSong();
     playSong();
 }
+// função que atualiza a barra de progresso conforme a musica passa
+function updateProgressBar() {
+    const barWidth = (song.currentTime/song.duration)*100;
+    progressBar.style.setProperty('--progress', `${barWidth}%`)
+}
+// função para avançar a musica
+function jumpTo(event) {
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)*song.duration;
+    song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray) {
+    const size = preShuffleArray.length;
+    let currentIndex = size - 1;
+    while(currentIndex > 0){
+        let randomIndex = Math.floor(Math.random()*size);
+        let aux = preShuffleArray[currentIndex];
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+        preShuffleArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+
+function shuffleButton() {
+    if(isShuffle == false){
+        isShuffle = true;
+        shuffleArray(sortedPlaylist);
+        shuffle.classList.add('button-active');
+    }
+    else{
+        isShuffle = false;
+        sortedPlaylist = [...playlist];
+        shuffle.classList.remove('button-active');
+    }
+}
 
 loadSong();
 
 play.addEventListener('click', playPause);
 previous.addEventListener('click',previousSong);
 next.addEventListener('click',nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
+shuffle.addEventListener('click', shuffleButton);
